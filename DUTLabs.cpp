d34e24ffd9efe1b86234456fb810c594049eee51
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-10-25 01:00:40 +0300

    lab4 done

--- DUTLabs.cpp
+++ DUTLabs.cpp
@@ -1,15 +1,18 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
 
 void problem1();
 void problem2();
+void problem3();
+void problem4();
 
 int main() {
 	int problemNumber;
 
-	cout << "Enter problem number (1, 2): ";
+	cout << "Enter problem number (1, 2, 3): ";
 	cin >> problemNumber;
 
 	switch (problemNumber) {
@@ -19,66 +22,78 @@ int main() {
 		case 2:
 			problem2();
 			break;
+		case 3:
+			problem3();
+			break;
+		case 4:
+			problem4();
+			break;
 	}
 }
 
 void problem1() {
-	int n;
-	cout << "Enter n: ";
-	cin >> n;
+	bool answer;
+	int value;
 
-	int firstDigit = n / 100;
-	int secondDigit = (n / 10) % 10;
-	int thirdDigit = n % 10;
+	cout << "Enter a value: ";
+	cin >> value;
 
-	int evenDigitsCount = 0;
-
-	if (firstDigit % 2 == 0) {
-		evenDigitsCount++;
-	}
-
-	if (secondDigit % 2 == 0) {
-		evenDigitsCount++;
-	}
-
-	if (thirdDigit % 2 == 0) {
-		evenDigitsCount++;
-	}
-
-	cout << "Even digits count: " << evenDigitsCount << endl;
+	answer = (value >= 0 && value < 10) || (value * 2 < 20 && value - 2 > -2) || (value - 1 > 1 && value / 2 < 10) || value == 111;
+	
+	cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
 }
 
 void problem2() {
-	// Ввести номер поїзда і вивести час його відправлення.
+	double var1, var2, var3, var4, var5;
 
-	int trainNumber;
-	cout << "Enter train number: ";
-	cin >> trainNumber;
+	cout << "Enter 5 numbers: ";
+	cin >> var1 >> var2 >> var3 >> var4 >> var5;
 
-	switch (trainNumber) {
-		case 1:
-			cout << "Train 1 departs at 8:00" << endl;
-			break;
-		case 2:
-			cout << "Train 2 departs at 9:00" << endl;
-			break;
-		case 3:
-			cout << "Train 3 departs at 10:00" << endl;
-			break;
-		case 4:
-			cout << "Train 4 departs at 11:00" << endl;
-			break;
-		case 5:
-			cout << "Train 5 departs at 12:00" << endl;
-			break;
-		case 6:
-			cout << "Train 6 departs at 13:00" << endl;
-			break;
-		case 7:
-			cout << "Train 7 departs at 14:00" << endl;
-			break;
-		default:
-			cout << "Invalid train number" << endl;
-			break;
+	cout << fixed << setprecision(1) << var1 << endl;
+	cout << fixed << setprecision(2) << var2 << endl;
+	cout << fixed << setprecision(6) << var3 << endl;
+	cout << fixed << setprecision(2) << var4 << endl;
+	cout << fixed << setprecision(0) << var5 << endl;
+}
+
+void problem3() {
+	int num1, num2;
+	float epsilon = 0.000001f;
+
+	cout << "Enter two numbers: ";
+	cin >> num1 >> num2;
+
+	if (num1 == 0 || num2 == 0) {
+		cout << "Cannot divide by zero" << endl;
+		exit(1);
+	}
+
+	float division1 = 1.0f / num1;
+	float division2 = 1.0f / num2;
+
+	if (abs(division1 - division2) < epsilon) {
+		cout << "Results are equal (by 0.000001 epsilon)" << endl;
+	}
+	else {
+		cout << "Results are not equal (by 0.000001 epsilon)" << endl;
+	}
+}
+
+void problem4() {
+	int num1, num2, num3, num4;
+
+	cout << "Enter four numbers: ";
+	cin >> num1 >> num2 >> num3 >> num4;
+
+	if (
+		num1 >= 1 && num1 <= 255 &&
+		num2 >= 1 && num2 <= 255 &&
+		num3 >= 1 && num3 <= 255 &&
+		num4 >= 1 && num4 <= 255
+		) {
+		cout << "IP Address: " << num1 << "." << num2 << "." << num3 << "." << num4 << endl;
+	}
+	else {
+		cout << "Invalid IP Address" << endl;
 	}
 }
