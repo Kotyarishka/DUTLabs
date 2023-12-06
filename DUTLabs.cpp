58587119be602c4a53a80fa39e751782685b58a6
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-12-06 17:38:20 +0200

    lab9

--- DUTLabs.cpp
+++ DUTLabs.cpp
@@ -1,15 +1,17 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
 void problem1();
 void problem2();
+void problem3();
+void problem4();
 
 int main()
 {
   string variant;
-  cout << "Enter number of the problem (1 or 2):";
+  cout << "Enter number of the problem (1, 2, 3 or 4): ";
 
   cin >> variant;
 
@@ -19,6 +21,12 @@ int main()
   else if (variant == "2") {
 		problem2();
   }
+  else if (variant == "3") {
+    problem3();
+  }
+  else if (variant == "4") {
+		problem4();
+	}
   else {
 		cout << "Wrong number of the problem" << endl;
 	}
@@ -26,35 +34,157 @@ int main()
 }
 
 void problem1() {
-  double a;
-  double b;
+  const int arrSize = 12;
 
-  cout << "Problem 1 solution" << endl;
-  
-  cout << "Enter a:";
-  cin >> a;
-  
-  cout << "Enter b:";
-  cin >> b;
+  int arr[arrSize]{};
 
-  double result = pow(0.315 * log(a) / (1 + pow(0.711 * log(b) / (1 + pow(log(b / a), 1.0 / 3)), 1.0 / 3)), 1.0 / 3);  
+  srand(time(NULL));
 
-  cout << "Result:" << result << endl;
+  for (int i = 0; i < arrSize; i++) {
+    arr[i] = rand() % 200 - 100;
+  }
+
+  cout << "Array: ";
+  for (int i = 0; i < arrSize; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  int max = arr[0];
+  int min = arr[0];
+
+  int maxIndex = 0;
+  int minIndex = 0;
+
+  for (int i = 0; i < arrSize; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      maxIndex = i;
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+      minIndex = i;
+    }
+  }
+
+  int temp = arr[maxIndex];
+  arr[maxIndex] = arr[minIndex];
+  arr[minIndex] = temp;
+
+  cout << "Array after swapping: ";
+  for (int i = 0; i < arrSize; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
 }
 
 void problem2() {
-  cout << "Problem 2 solution" << endl;
+  int a;
 
-  double a;
-  double b;
-
-  cout << "Enter a:";
+  cout << "Enter A: ";
   cin >> a;
 
-  cout << "Enter b:";
-  cin >> b;
+  const int arrSize = 12;
 
-  double result = 8.15 * pow(b, 1.0 / 3) * log(a) / (24.38 * cos(b) * (exp(a) - pow(a, a)));
+  int arr[arrSize]{};
 
-  cout << "Result:" << result << endl;
+  srand(time(NULL));
+
+  for (int i = 0; i < arrSize; i++) {
+    arr[i] = rand() % 200 - 100;
+  }
+
+  cout << "Array: ";
+  for (int i = 0; i < arrSize; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  int elementsGreaterThanA = 0;
+
+  for (int i = 0; i < arrSize; i++) {
+    if (arr[i] > a) {
+			elementsGreaterThanA++;
+		}
+	}
+
+  cout << "Elements greater than A: " << elementsGreaterThanA << endl;
+}
+
+void problem3() {
+  const int arrSize = 12;
+
+  int arr[arrSize]{};
+
+  srand(time(NULL));
+
+  for (int i = 0; i < arrSize; i++) {
+    arr[i] = rand() % 200 - 100;
+  }
+
+  cout << "Array: ";
+  for (int i = 0; i < arrSize; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  int max = arr[0];
+  int maxIndex = 0;
+
+  for (int i = 0; i < arrSize; i++) {
+    if (arr[i] > max) {
+			max = arr[i];
+			maxIndex = i;
+		}
+	}
+
+  // remove max element from array
+  remove(arr, arr + arrSize, max);
+
+  max = arr[0];
+  
+  for (int i = 0; i < arrSize - 1; i++) {
+    if (arr[i] > max) {
+			max = arr[i];
+		}
+  }
+
+  cout << "Second max element: " << max << endl;
+}
+
+void problem4() {
+  const int arrSize = 12;
+
+  int arr[arrSize]{};
+
+  srand(time(NULL));
+
+  for (int i = 0; i < arrSize; i++) {
+    arr[i] = rand() % 200 - 100;
+  }
+
+  cout << "Array: ";
+  for (int i = 0; i < arrSize; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  int zeroIndex = 0;
+  bool zeroFound = false;
+  for (int i = 0; i < arrSize; i++) {
+    if (arr[i] == 0) {
+			zeroIndex = i;
+			zeroFound = true;
+			break;
+		}
+	}
+	
+  if (zeroFound) {
+		cout << "Zero index: " << zeroIndex << endl;
+	}
+  else {
+		cout << "Zero not found" << endl;
+	}
 }
