498266698ccd6a7e5564e0e6310095cbf265bbd6
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-12-06 17:17:49 +0200

    init

--- DUTLabs.cpp
+++ DUTLabs.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -9,7 +8,7 @@ void problem2();
 int main()
 {
   string variant;
-  cout << "Enter number of the problem (1 or 2):";
+  cout << "Enter number of the problem (1 or 2): ";
 
   cin >> variant;
 
@@ -25,36 +24,99 @@ int main()
 
 }
 
+struct Time {
+  int hours;
+  int minutes;
+};
+
 void problem1() {
-  double a;
-  double b;
+  Time time{};
 
-  cout << "Problem 1 solution" << endl;
-  
-  cout << "Enter a:";
-  cin >> a;
-  
-  cout << "Enter b:";
-  cin >> b;
+  int minutesToAdd = 0;
 
-  double result = pow(0.315 * log(a) / (1 + pow(0.711 * log(b) / (1 + pow(log(b / a), 1.0 / 3)), 1.0 / 3)), 1.0 / 3);  
+  cout << "Enter hours: ";
+  cin >> time.hours;
 
-  cout << "Result:" << result << endl;
+  cout << "Enter minutes: ";
+  cin >> time.minutes;
+
+  cout << "Enter minutes to add: ";
+  cin >> minutesToAdd;
+
+  if (time.hours > 24 || time.hours < 0) {
+    cout << "Wrong hours, must be between 0 and 24" << endl;
+    return;
+  }
+
+  if (time.minutes > 60 || time.minutes < 0) {
+		cout << "Wrong minutes, must be between 0 and 60" << endl;
+    return;
+	}
+
+	time.minutes += minutesToAdd;
+
+  if (time.minutes > 60) {
+		time.hours += time.minutes / 60;
+		time.minutes %= 60;
+	}
+
+  if (time.hours > 24) {
+		time.hours %= 24;
+	}
+
+	cout << "Time: " << time.hours << ":" << time.minutes << endl;
 }
 
 void problem2() {
-  cout << "Problem 2 solution" << endl;
+  Time startTime{};
+  Time endTime{};
 
-  double a;
-  double b;
+  cout << "Enter start time hours: ";
+  cin >> startTime.hours;
 
-  cout << "Enter a:";
-  cin >> a;
+  cout << "Enter start time minutes: ";
+  cin >> startTime.minutes;
 
-  cout << "Enter b:";
-  cin >> b;
+  cout << "Enter end time hours: ";
+  cin >> endTime.hours;
 
-  double result = 8.15 * pow(b, 1.0 / 3) * log(a) / (24.38 * cos(b) * (exp(a) - pow(a, a)));
+  cout << "Enter end time minutes: ";
+  cin >> endTime.minutes;
 
-  cout << "Result:" << result << endl;
+  if (startTime.hours > 24 || startTime.hours < 0) {
+		cout << "Wrong start time hours, must be between 0 and 24" << endl;
+		return;
+	}
+
+  if (startTime.minutes > 60 || startTime.minutes < 0) {
+		cout << "Wrong start time minutes, must be between 0 and 60" << endl;
+		return;
+	}
+
+  if (endTime.hours > 24 || endTime.hours < 0) {
+		cout << "Wrong end time hours, must be between 0 and 24" << endl;
+		return;
+	}
+
+  if (endTime.minutes > 60 || endTime.minutes < 0) {
+		cout << "Wrong end time minutes, must be between 0 and 60" << endl;
+		return;
+	}
+
+	int hours = endTime.hours - startTime.hours;
+	int minutes = endTime.minutes - startTime.minutes;
+
+
+  if (minutes < 0) {
+		hours--;
+		minutes += 60;
+	}
+
+  if (hours < 0) {
+    cout << "Wrong time, end time must be after start time" << endl;
+    return;
+  }
+
+	cout << "Time: " << hours << ":" << minutes << endl;
 }
+
